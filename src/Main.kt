68b5828b9f1abd19bf5fee327cbe89a9a3dde988
Carransah7 <brandoncarransah@gmail.com>
2025-02-16 21:24:07 -0500

    add more possible calculations

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,31 @@
 import kotlin.system.exitProcess
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 fun main() {
     println("***  Console Calculator  ***")
 
     //input collection
-    println("\nEnter two numbers:\n")
-    val number1 = readDoubleInput("Number 1: ")
-    val number2 = readDoubleInput("Number 2: ")
+    println("\nEnter two numbers (or one number for square root):\n")
+    val number1 = readDoubleInput("First number: ")
+    var number2 = 0.0
 
     //operation selection
     showChoices()
     val operation = getArithmeticOperation()
+    if(operation != "√") {
+        number2 = readDoubleInput("\nSecond number: ")
+    }
 
     //calculation
     val result = performCalculation(number1, number2, operation)
 
     //result display
-    println("\nResult:\n" +
-            "$number1 $operation $number2 = $result")
+    if(operation == "√") {
+        println("\nResult:\n√$number1 = $result")
+    } else {
+        println("\nResult:\n" + "$number1 $operation $number2 = $result")
+    }
 }
 
 fun readDoubleInput(prompt: String): Double {
@@ -39,13 +47,16 @@ fun showChoices() {
     println("2. Subtraction (-)")
     println("3. Multiplication (*)")
     println("4. Division (/)")
+    println("5. Modulo (%)")
+    println("5. Power (x^n)")
+    println("5. Square root (√)")
 }
 
 fun getArithmeticOperation(): String {
-    print("\nEnter an arithmetic operation (+, -, *, /): ")
+    print("\nEnter an arithmetic operation (+, -, *, /, %, ^, √): ")
     val operation = readln()
 
-    if(!"+-*/".contains(operation, true)){
+    if(!"+-*/%^√".contains(operation, true)){
         println("\nInvalid operation. Exiting.")
         exitProcess(2) //exit the program
     }
@@ -57,14 +68,25 @@ fun performCalculation(number1: Double, number2: Double, operation: String): Dou
         "+" -> number1 + number2
         "-" -> number1 - number2
         "*" -> number1 * number2
-        "/" -> if (number2 != 0.0) number1 / number2
+        "/" -> if(number2 != 0.0) number1 / number2
         else {
             println("\nDivision by zero is not allowed. Exiting.")
             exitProcess(3)
         }
+        "%" -> if(number2 != 0.0) number1 % number2
+        else {
+            println("\nModulo by zero is not allowed. Exiting.")
+            exitProcess(4)
+        }
+        "^" -> number1.pow(number2)
+        "√" -> if(number1 >= 0) sqrt(number1)
+        else {
+            println("\nSquare root of a negative number is not allowed. Exiting.")
+            exitProcess(5)
+        }
         else -> {
             println("\nUnexpected error encountered. Exiting.")
-            exitProcess(4)
+            exitProcess(6)
         }
     }
 }
