133e52a9917de9494fbcbe9c5b337a33db5d9634
Carransah7 <brandoncarransah@gmail.com>
2025-02-16 20:25:43 -0500

    basic calculator

---
+++ src/Main.kt
@@ -0,0 +1,70 @@
+import kotlin.system.exitProcess
+
+fun main() {
+    println("***  Console Calculator  ***")
+
+    //input collection
+    println("\nEnter two numbers:\n")
+    val number1 = readDoubleInput("Number 1: ")
+    val number2 = readDoubleInput("Number 2: ")
+
+    //operation selection
+    showChoices()
+    val operation = getArithmeticOperation()
+
+    //calculation
+    val result = performCalculation(number1, number2, operation)
+
+    //result display
+    println("\nResult:\n" +
+            "$number1 $operation $number2 = $result")
+}
+
+fun readDoubleInput(prompt: String): Double {
+    print(prompt)
+    val num = readln()
+
+    //check input validity
+    try {
+        return num.toDouble()
+    } catch (e: Exception) {
+        println("Error reading input: ${e.message}")
+        exitProcess(1)
+    }
+}
+
+fun showChoices() {
+    println("\nOperation Options:")
+    println("1. Addition (+)")
+    println("2. Subtraction (-)")
+    println("3. Multiplication (*)")
+    println("4. Division (/)")
+}
+
+fun getArithmeticOperation(): String {
+    print("\nEnter an arithmetic operation (+, -, *, /): ")
+    val operation = readln()
+
+    if(!"+-*/".contains(operation, true)){
+        println("\nInvalid operation. Exiting.")
+        exitProcess(2) //exit the program
+    }
+    return operation
+}
+
+fun performCalculation(number1: Double, number2: Double, operation: String): Double {
+    return when (operation) {
+        "+" -> number1 + number2
+        "-" -> number1 - number2
+        "*" -> number1 * number2
+        "/" -> if (number2 != 0.0) number1 / number2
+        else {
+            println("\nDivision by zero is not allowed. Exiting.")
+            exitProcess(3)
+        }
+        else -> {
+            println("\nUnexpected error encountered. Exiting.")
+            exitProcess(4)
+        }
+    }
+}
